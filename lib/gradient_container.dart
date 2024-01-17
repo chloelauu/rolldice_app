a95d0495abc78be5048c9c54034f415b71a681e0
chlv33 <mabinogigames7@gmail.com>
2024-01-17 21:08:10 +0800

    addproject

---
+++ lib/gradient_container.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:rolldice_app/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.pink({super.key})
+      : color1 = const Color(0xFFAF6480),
+        color2 = const Color(0xFFF6B2E1);
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:rolldice_app/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer.pink(),
+      ),
+    ),
+  );
+}
